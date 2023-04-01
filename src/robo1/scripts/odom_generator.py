d7366379553feb2c863e15da1f9f0efaa9905d16
Vedant Ranade <vedantr1@umd.edu>
2023-04-01 23:07:39 +0000

    odom calculator minor improvements

--- src/robo1/scripts/odom_generator.py
+++ src/robo1/scripts/odom_generator.py
@@ -5,9 +5,13 @@ from geometry_msgs.msg import Pose2D
 import numpy as np
 import math
 
-timeStep = 0.02 #50 Hz
 r = 6.45/2
 L = 11
+flag = True
+flagVel = True
+
+velX_old = 0
+velY_old = 0
 
 # global variable to store the current odometry values
 curr_odom = Pose2D()
@@ -17,26 +21,55 @@ curr_odom.y = 0.0
 curr_odom.theta = 0.0
 
 def curr_vel_callback(data):
+    global timeGap
+    global velX_old
+    global velY_old
+    global flagVel
+
+    # Old velocity at first run is not important
+    if flagVel:
+        velX_old = data.x
+        velY_old = data.y
+        flagVel = False
+
     # Read velocity data from '/CURR_VEL' topic
     vel = data
 
-    velX = round(vel[0]*5)/5
-    velY = round(vel[1]*5)/5
+    # Some wierd rounding method by me
+    velX = vel.x #round(vel.x*5)/5
+    velY = vel.y #round(vel.y*5)/5
+
+    if abs(velX-velX_old) > abs(velX) and velX!=0 and velX_old!=0:
+        velX = velX_old
+    if abs(velY-velY_old) > abs(velY) and velY!=0 and velY_old!=0:
+        velY = velY_old
 
     # Add velocity values to current odometry values
-    curr_odom.x = curr_odom.x - (((6.45/2)/2)* (velX + velY)*np.cos(curr_odom.theta))*0.02
-    curr_odom.y = curr_odom.y - (((6.45/2)/2)* (velX + velY)*np.sin(curr_odom.theta))*0.02
-    curr_odom.theta = curr_odom.theta + (((6.45/2)/11)*(velX - velY))*0.02
+    rate = rospy.get_time() - timeGap
 
-    curr_odom.theta = ((curr_odom.theta + math.pi)%(2*math.pi)) - math.pi
+    curr_odom.x = curr_odom.x + (((6.45/2)/2)* (velX + velY)*np.cos(curr_odom.theta))* rate
+    curr_odom.y = curr_odom.y + (((6.45/2)/2)* (velX + velY)*np.sin(curr_odom.theta))* rate
+    curr_odom.theta = curr_odom.theta + (((6.45/2)/11)*(velX - velY))* rate
+
+    #curr_odom.theta = ( curr_odom.theta % 2*math.pi )
+
+    timeGap = rospy.get_time()
 
     # Publish updated Pose2D message to '/CURR_ODOM' topic
     curr_odom_publisher.publish(curr_odom)
 
+    velX_old = velX
+    velY_old = velY
+
 
 if __name__ == '__main__':
+
     # Initialize ROS node
     rospy.init_node('odom_vel_addition', anonymous=True)
+
+    if flag:
+        timeGap = rospy.get_time()
+        flag = False
 
     # Create publisher for '/CURR_ODOM' topic with 'Pose2D' message type
     curr_odom_publisher = rospy.Publisher('/CURR_ODOM', Pose2D, queue_size=10)
