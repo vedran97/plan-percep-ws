4b9c8c51fa0d0ed5c251de8c13355048d29a31ea
Vedant Ranade <vedantr1@umd.edu>
2023-04-06 03:10:19 +0000

    Found tuning gains for all rotation and translation

--- src/rosserial_terpbot/src/nodes/TargetPublisher.py
+++ src/rosserial_terpbot/src/nodes/TargetPublisher.py
@@ -3,17 +3,17 @@
 from time import sleep
 import rospy
 from geometry_msgs.msg import Pose2D
-from control_toolbox.srv import SetPidGainsRequest
+from terpbot_msgs.msg import Gains
 from std_msgs.msg import Bool
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 PUB_TOPIC = 'TARG_VEL'
 GAINS_TOPIC = 'GAINS'
 
 rospy.init_node('TARGET_PUBLISHER')
 pose_publisher = rospy.Publisher(PUB_TOPIC, Pose2D, queue_size=10)
-gains_publisher = rospy.Publisher(GAINS_TOPIC, SetPidGainsRequest, queue_size=10)
+gains_publisher = rospy.Publisher(GAINS_TOPIC, Gains, queue_size=10)
 rospy.loginfo("Publishes target and gains to arduino")
 
 NO_OF_WAYPOINTS = 350
@@ -21,7 +21,7 @@ CONTROL_FREQ = 50
 TICKS_PER_REV= 495
 DOUBLE_PI = 2*np.pi
 DIA_WHEEL = 64.5/1000 #(converting it to meter)
-MAX_RPM = 140
+MAX_RPM = 80
 VMAX = (MAX_RPM/60)*DOUBLE_PI*(DIA_WHEEL/2)
 
 print("\r\nVMAX:{}".format(VMAX))
@@ -48,7 +48,7 @@ class Trajectory:
         loopRate = CONTROL_FREQ
         timeIncrement = 1/loopRate
         xInit = 0;  
-        xFinal = 3.0; #// 1 rotation of the wheel //0.20263272615
+        xFinal = 1.0; #// 1 rotation of the wheel //0.20263272615
         self.totalTime = getT(xInit,xFinal)
         i=0
         while (True):
@@ -63,12 +63,12 @@ class Trajectory:
 
 wayPoints = Trajectory()
 
-print("\r\nTOTAL_TRAJ_TIME , NO_OF_WAYPOINTS:{},{}".format(wayPoints.totalTime,wayPoints.noOfWayPoints))
+print("\r\nTOTAL_TRAJ_TIME , NO_OF_WAYPOINTS:{},{}\r\n".format(wayPoints.totalTime,wayPoints.noOfWayPoints))
 
 
 # Create a new figure, plot into it, then close it so it never gets displayed
 
-plt.ioff()
+# plt.ioff()
 
 # Create a new figure, plot into it, then close it so it never gets displayed
 # fig = plt.figure()
@@ -83,17 +83,56 @@ pose_msg.theta = 0 # Empty Array
 
 rate = rospy.Rate(CONTROL_FREQ)
 
-leftGain = SetPidGainsRequest()
-leftGain.p = 5
-leftGain.i = 0
-leftGain.d = 1
-leftGain.antiwindup = True
+## ROTATION GAINS:
+# left_KU = 35
+# left_TU = 0.1481
+# left_kd_coeff = 0.085
 
+# right_KU = 42
+# right_TU = 0.1481
+# right_kd_coeff = 0.085
+
+# TRANSLATION GAINS:
+left_KU = 32.5
+left_TU = 0.1481
+left_kd_coeff = 0.085
+
+right_KU = 38
+right_TU = 0.1481
+right_kd_coeff = 0.085
+
+leftGain = Gains()
+
+leftGain.kp = 0.6*left_KU
+leftGain.ki = 1.2*left_KU/left_TU
+leftGain.kd = left_kd_coeff*left_KU*left_TU
+leftGain.i_clamp = 100.0
+leftGain.isleft = True
+
+
+rightGain = Gains()
+rightGain.kp =  0.6*right_KU
+rightGain.ki = 1.2*right_KU/left_TU
+rightGain.kd = right_kd_coeff*right_KU*right_TU
+rightGain.i_clamp = 100.0
+rightGain.isleft = False
+
+# '{kp: 30.0, ki: 0.05, kd: 0.1, i_clamp: 100.0, isleft: false}
 
 while not rospy.is_shutdown():
     rospy.loginfo("Sending Gains:")
-    gains_publisher.publish(leftGain)
-    sleep(1)
+    rospy.loginfo("Left Motor Gains:\r\n{}".format(leftGain))
+    rospy.loginfo("Right Motor Gains:\r\n{}".format(rightGain))
+
+    for i in range(20):
+        gains_publisher.publish(leftGain)
+        rate.sleep()
+
+    for i in range(20):
+        gains_publisher.publish(rightGain)
+        rate.sleep()
+
+
     rospy.loginfo("Finished sending gains")
     pose_msg.x = 0
     pose_msg.y = 0
@@ -102,13 +141,15 @@ while not rospy.is_shutdown():
     sleep(1)
     for wp in wayPoints.wayPoints:
         pose_msg.x = wp
-        pose_msg.y = wp
+        pose_msg.y = +wp
         pose_msg.theta = 1
         pose_publisher.publish(pose_msg)
         rate.sleep()
-    print("\r\nSTOPPING THE TRAJGEN NOW")
-    pose_msg.theta = 0
-    pose_publisher.publish(pose_msg)
+    print("\r\nSTOPPING THE TRAJGEN NOW\r\n")
+    for i in range(20):
+        pose_msg.theta = 0
+        pose_publisher.publish(pose_msg)
+        rate.sleep()
     sleep(1)
     break
 
