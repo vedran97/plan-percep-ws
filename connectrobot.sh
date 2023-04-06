b025f9fce7d2ab44a78100ba967317713cc5465d
shyam-pi <pi.shyamsundar@gmail.com>
2023-04-06 01:32:43 +0000

    changed script for different machines

--- connectrobot.sh
+++ connectrobot.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
 
+# For Vedant
 alias myip="ip addr show wlp9s0 | grep -Eo 'inet ([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{print \$2}'"
 
-export ROS_MASTER_URI=http://10.104.116.73:11311
+# For shyam
+alias myip="ip addr show ens33 | grep -Eo 'inet ([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{print \$2}'"
+
+export ROS_MASTER_URI=http://10.104.119.60:11311
 
 export ROS_IP=$(myip)
 export ROS_HOSTNAME=$(myip)
