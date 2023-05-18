50b47e387f469575747763705ddd004da6efa2a7
vedran97 <vedantmranade@gmail.com>
2023-05-18 21:08:09 +0000

    Merge errors

--- connectrobot.sh
+++ connectrobot.sh
@@ -5,8 +5,7 @@ alias myip="ip addr show wlp9s0 | grep -Eo 'inet ([0-9]{1,3}\.){3}[0-9]{1,3}' | 
 
 # For shyam
 # alias myip="ip addr show wlo1 | grep -Eo 'inet ([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{print \$2}'"
-
-export ROS_MASTER_URI=http://10.104.115.189:11311
-
+export TURTLERBOT3_MODEL=burger
+export ROS_MASTER_URI=http://10.104.84.112:11311
 export ROS_IP=$(myip)
 export ROS_HOSTNAME=$(myip)
